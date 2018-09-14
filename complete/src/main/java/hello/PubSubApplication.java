3ff5fdc9009cb4953c1ae958e906b8c19c30db22
flavio <fdsdev@gmail.com>
2018-09-14 07:37:44 -0300

    passing message header

--- complete/src/main/java/hello/PubSubApplication.java
+++ complete/src/main/java/hello/PubSubApplication.java
@@ -1,7 +1,5 @@
 package hello;
 
-import java.io.IOException;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -13,74 +11,61 @@ import org.springframework.cloud.gcp.pubsub.integration.inbound.PubSubInboundCha
 import org.springframework.cloud.gcp.pubsub.integration.outbound.PubSubMessageHandler;
 import org.springframework.cloud.gcp.pubsub.support.GcpPubSubHeaders;
 import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.GatewayHeader;
 import org.springframework.integration.annotation.MessagingGateway;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.channel.DirectChannel;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.handler.annotation.Header;
 
 import com.google.cloud.pubsub.v1.AckReplyConsumer;
 
 @SpringBootApplication
 public class PubSubApplication {
 
-  private static final Log LOGGER = LogFactory.getLog(PubSubApplication.class);
+	private static final Log LOGGER = LogFactory.getLog(PubSubApplication.class);
 
-  public static void main(String[] args) throws IOException {
-    SpringApplication.run(PubSubApplication.class, args);
-  }
+	public static void main(String[] args) {
+		SpringApplication.run(PubSubApplication.class, args);
+	}
 
-  // Inbound channel adapter.
+	@Bean
+	public MessageChannel pubsubInputChannel() {
+		return new DirectChannel();
+	}
 
-  // tag::pubsubInputChannel[]
-  @Bean
-  public MessageChannel pubsubInputChannel() {
-    return new DirectChannel();
-  }
-  // end::pubsubInputChannel[]
+	@Bean
+	public PubSubInboundChannelAdapter messageChannelAdapter(
+			@Qualifier("pubsubInputChannel") MessageChannel inputChannel, PubSubOperations pubSubTemplate) {
+		PubSubInboundChannelAdapter adapter = new PubSubInboundChannelAdapter(pubSubTemplate, "testSubscription");
+		adapter.setOutputChannel(inputChannel);
+		adapter.setAckMode(AckMode.MANUAL);
 
-  // tag::messageChannelAdapter[]
-  @Bean
-  public PubSubInboundChannelAdapter messageChannelAdapter(
-      @Qualifier("pubsubInputChannel") MessageChannel inputChannel,
-      PubSubOperations pubSubTemplate) {
-    PubSubInboundChannelAdapter adapter =
-        new PubSubInboundChannelAdapter(pubSubTemplate, "testSubscription");
-    adapter.setOutputChannel(inputChannel);
-    adapter.setAckMode(AckMode.MANUAL);
+		return adapter;
+	}
 
-    return adapter;
-  }
-  // end::messageChannelAdapter[]
+	@Bean
+	@ServiceActivator(inputChannel = "pubsubInputChannel")
+	public MessageHandler messageReceiverClient() {
+		return message -> {
+			LOGGER.info("clientId: " + message.getHeaders().get("clientId"));
+			LOGGER.info("orgId: " + message.getHeaders().get("orgId"));
+			LOGGER.info("message: " + message.getPayload());
+			AckReplyConsumer consumer = (AckReplyConsumer) message.getHeaders().get(GcpPubSubHeaders.ACKNOWLEDGEMENT);
+			consumer.ack();
+		};
+	}
 
-  // tag::messageReceiver[]
-  @Bean
-  @ServiceActivator(inputChannel = "pubsubInputChannel")
-  public MessageHandler messageReceiver() {
-    return message -> {
-      LOGGER.info("Message arrived! Payload: " + message.getPayload());
-      AckReplyConsumer consumer =
-          (AckReplyConsumer) message.getHeaders().get(GcpPubSubHeaders.ACKNOWLEDGEMENT);
-      consumer.ack();
-    };
-  }
-  // end::messageReceiver[]
+	@Bean
+	@ServiceActivator(inputChannel = "pubsubOutputChannel")
+	public MessageHandler messageSender(PubSubOperations pubsubTemplate) {
+		return new PubSubMessageHandler(pubsubTemplate, "testTopic");
+	}
 
-  // Outbound channel adapter
+	@MessagingGateway(defaultRequestChannel = "pubsubOutputChannel", defaultHeaders = @GatewayHeader(name = "orgId", value = "1"))
+	public interface PubsubOutboundGateway {
 
-  // tag::messageSender[]
-  @Bean
-  @ServiceActivator(inputChannel = "pubsubOutputChannel")
-  public MessageHandler messageSender(PubSubOperations pubsubTemplate) {
-    return new PubSubMessageHandler(pubsubTemplate, "testTopic");
-  }
-  // end::messageSender[]
-
-  // tag::messageGateway[]
-  @MessagingGateway(defaultRequestChannel = "pubsubOutputChannel")
-  public interface PubsubOutboundGateway {
-
-    void sendToPubsub(String text);
-  }
-  // end::messageGateway[]
+		void sendToPubsub(@Header("clientId") String clientId, String text);
+	}
 }
--- complete/src/main/java/hello/WebAppController.java
+++ complete/src/main/java/hello/WebAppController.java
@@ -1,23 +1,23 @@
 package hello;
 
-import hello.PubSubApplication.PubsubOutboundGateway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
+import hello.PubSubApplication.PubsubOutboundGateway;
+
 @RestController
 public class WebAppController {
 
-  // tag::autowireGateway[]
-  @Autowired
-  private PubsubOutboundGateway messagingGateway;
-  // end::autowireGateway[]
+	@Autowired
+	private PubsubOutboundGateway messagingGateway;
 
-  @PostMapping("/publishMessage")
-  public RedirectView publishMessage(@RequestParam("message") String message) {
-    messagingGateway.sendToPubsub(message);
-    return new RedirectView("/");
-  }
+	@PostMapping("/publishMessage")
+	public RedirectView publishMessage(@RequestParam("clientId") String clientId, @RequestParam("message") String message) {
+		messagingGateway.sendToPubsub(clientId, message);
+		return new RedirectView("/");
+	}
+	
 }
